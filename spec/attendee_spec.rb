f3db297f4bcc8739015c5b2bfb8c59b12b269ec4
Kam Kennedy <kameronk013@gmail.com>
2023-09-26 09:17:55 -0600

    complete add bid method and spec

--- spec/attendee_spec.rb
+++ spec/attendee_spec.rb
@@ -2,14 +2,22 @@ require "./spec/spec_helper"
 
 RSpec.describe Attendee do
   before(:each) do
-    @attendee = Attendee.new({name: 'Megan', budget: '$50'})
+    # @item1 = Item.new('Chalkware Piggy Bank')
+    # @item2 = Item.new('Bamboo Picture Frame')
+    # @item3 = Item.new('Homemade Chocolate Chip Cookies')
+    # @item4 = Item.new('2 Days Dogsitting')
+    # @item5 = Item.new('Forever Stamps')
+    @attendee1 = Attendee.new({name: 'Megan', budget: '$50'})
+    # @attendee2 = Attendee.new({name: 'Bob', budget: '$75'})
+    # @attendee3 = Attendee.new({name: 'Mike', budget: '$100'})
+    # @auction = Auction.new
   end
 
   describe '#initialize' do
     it 'exists' do
-      expect(@attendee).to be_an_instance_of(Attendee)
-      expect(@attendee.name).to eq("Megan")
-      expect(@attendee.budget).to eq("$50")
+      expect(@attendee1).to be_an_instance_of(Attendee)
+      expect(@attendee1.name).to eq("Megan")
+      expect(@attendee1.budget).to eq("$50")
     end
   end
 end
--- spec/auction_spec.rb
+++ spec/auction_spec.rb
@@ -2,6 +2,14 @@ require "./spec/spec_helper"
 
 RSpec.describe Auction do
   before(:each) do
+    @item1 = Item.new('Chalkware Piggy Bank')
+    @item2 = Item.new('Bamboo Picture Frame')
+    @item3 = Item.new('Homemade Chocolate Chip Cookies')
+    @item4 = Item.new('2 Days Dogsitting')
+    @item5 = Item.new('Forever Stamps')
+    @attendee1 = Attendee.new({name: 'Megan', budget: '$50'})
+    @attendee2 = Attendee.new({name: 'Bob', budget: '$75'})
+    @attendee3 = Attendee.new({name: 'Mike', budget: '$100'})
     @auction = Auction.new
   end
 
@@ -9,6 +17,13 @@ RSpec.describe Auction do
     it 'exists' do
       expect(@auction).to be_an_instance_of(Auction)
       expect(@auction.items).to eq([])
+    end
+  end
+
+  describe "#add_item" do
+    it "adds an item to the auction" do
+      @auction.add_item(@item1)
+      @auction.add_item(@item2)
     end
   end
 end
